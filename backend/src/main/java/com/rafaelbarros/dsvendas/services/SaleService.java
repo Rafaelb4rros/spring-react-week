1a0d0f785e432c01a6187dcc2369af92de637740
Rafael Barros <rafael33041@gmail.com>
2021-11-03 22:19:24 -0300

    Pagination

---
+++ backend/src/main/java/com/rafaelbarros/dsvendas/services/SaleService.java
@@ -0,0 +1,29 @@
+package com.rafaelbarros.dsvendas.services;
+
+import com.rafaelbarros.dsvendas.dto.SaleDTO;
+import com.rafaelbarros.dsvendas.entities.Sale;
+import com.rafaelbarros.dsvendas.repositories.SaleRepository;
+import com.rafaelbarros.dsvendas.repositories.SellerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SaleService {
+
+  @Autowired
+  private SaleRepository repository;
+
+  @Autowired
+  private SellerRepository sellerRepository;
+
+  @Transactional(readOnly = true)
+  public Page<SaleDTO> findAll(Pageable pageable) {
+    sellerRepository.findAll();
+    Page<Sale> result = repository.findAll(pageable);
+    return result.map(x -> new SaleDTO(x));
+  }
+}
